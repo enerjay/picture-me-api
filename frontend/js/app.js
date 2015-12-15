46cd54ae3864c9a69b030fee884f4bda9fb18dbe
enerjay <enerjay@gmail.com>
2015-12-15 12:24:51 +0000

    set up ui router - masonry not working

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,7 +1,9 @@
 angular
-  .module('picturemeApp', ['satellizer'])
+  .module('picturemeApp', ['satellizer', 'ui.router'])
   .constant('API_URL', 'http://localhost:3000')
-  .config(oauthConfig);
+  .config(oauthConfig)
+  .config(MainRouter);
+
 
 oauthConfig.$inject = ['API_URL', '$authProvider'];
 function oauthConfig(API_URL, $authProvider) {
@@ -9,4 +11,38 @@ function oauthConfig(API_URL, $authProvider) {
     url: API_URL + '/auth/facebook',
     clientId: '1535623983421042'
   });
+}
+
+function MainRouter($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('index', { //'home is a name ---so we canrefer to this stste later
+      url: "", // a relative url -- so angular can match the route to this state. 
+      templateUrl: "home.html" // the name of the file that contains our html for this state (a template)
+    })
+    .state('home', { //'home is a name ---so we canrefer to this stste later
+      url: "/", // a relative url -- so angular can match the route to this state. 
+      templateUrl: "home.html" // the name of the file that contains our html for this state (a template)
+    })
+    .state('add_photos', {
+      url: "/add_photos", 
+      templateUrl: "add_photos.html"
+    })
+    .state('submit_photos', {
+      url: "/submit_photos", 
+      templateUrl: "submit_photos.html"
+    })
+    .state('picture_me', {
+      url: "/picture_me",
+      templateUrl: "picture_me.html"
+    })
+    .state('share', {
+      url: "/share", 
+      templateUrl: "share.html"
+    })
+    .state('about', {
+      url: "/about", 
+      templateUrl: "about.html"
+    });
+
+  $urlRouterProvider.otherwise("/");
 }
