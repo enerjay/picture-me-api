92aebb6d0f53386d9832ba3db8e0967f4faa8006
enerjay <enerjay@gmail.com>
2015-12-17 10:24:05 +0000

    pushing to heroku

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  databaseUrl: 'mongodb://localhost/pictureme',
+  databaseUrl: process.env.MONGOLAB_URI || 'mongodb://localhost/pictureme',
   secret: 'iLoveMakingPictures',
-  appUrl: 'http://localhost:8000',
+  appUrl: process.env.FRONT_END_URL || 'http://localhost:8000',
   oauth: {
     facebook: {
       accessTokenUrl: 'https://graph.facebook.com/v2.5/oauth/access_token',
