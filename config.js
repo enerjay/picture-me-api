af32c858283da0ee8922b7be739f166bafc2375d
enerjay <enerjay@gmail.com>
2016-01-20 18:00:00 +0000

    change the domain name for faceboo login

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   port: process.env.PORT || 3000,
   databaseUrl: process.env.MONGOLAB_URI || 'mongodb://localhost/pictureme',
   secret: 'iLoveMakingPictures',
-  appUrl: 'http://enerjay.github.io',
+  appUrl: 'http://jhepburn-zikalala.com',
   oauth: {
     facebook: {
       accessTokenUrl: 'https://graph.facebook.com/v2.5/oauth/access_token',
