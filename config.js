a28f51619dc6b490d8ca8a2f8537de11091eb6a0
enerjay <enerjay@gmail.com>
2016-01-20 18:05:15 +0000

    test url

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   port: process.env.PORT || 3000,
   databaseUrl: process.env.MONGOLAB_URI || 'mongodb://localhost/pictureme',
   secret: 'iLoveMakingPictures',
-  appUrl: 'http://jhepburn-zikalala.com',
+  appUrl: 'http://enerjay.github.io',
   oauth: {
     facebook: {
       accessTokenUrl: 'https://graph.facebook.com/v2.5/oauth/access_token',
